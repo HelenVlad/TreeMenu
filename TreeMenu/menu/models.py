79a14609677dd9ab990b5dbe8f690433d5dc0f24
HelenVlad <eilnw.felix@gmail.com>
2024-04-10 21:15:58 +0500

    develop. Add models

---
+++ TreeMenu/menu/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class MenuItems(models.Model):
+    name_item = models.CharField(max_length=200)
+    higher_level = models.ForeignKey('self', null=True, blank=True, related_name='lower_level',
+                                     on_delete=models.CASCADE)
+    url = models.CharField(max_length=200)
+    named_url = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.name_item
